8810ba911f777779f4ea0d023eb0f374d246201b
CodeByDex <119005046+CodeByDex@users.noreply.github.com>
2022-11-27 16:29:01 -0500

    Completed section 3.3.4

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
-var topics = "HTML, CSS, Git, Javascript";
+var topic = "HTML";
 
-console.log(topics);
+if (topic === "HTML"){
+    console.log("Let's study HTML");
+} else if (topic === "CSS") {
+    console.log("Let's study CSS");
+} else if (topic === "Git") {
+    console.log("Let's study Git");
+} else if (topic === "Javascript") {
+    console.log("Let's study Javascript");
+} else {
+    console.log("Try Again");
+}
