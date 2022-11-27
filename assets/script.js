69990c378083877cabecdaa0a10ccbc6d96802df
CodeByDex <119005046+CodeByDex@users.noreply.github.com>
2022-11-27 17:27:11 -0500

    Completed module 3.4

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,12 @@
-
 function selectTopic() {
 
-    if (topic === "HTML"){
+    if (topics[randomTopic] === "HTML"){
         console.log("Let's study HTML");
-    } else if (topic === "CSS") {
+    } else if (topics[randomTopic] === "CSS") {
         console.log("Let's study CSS");
-    } else if (topic === "Git") {
+    } else if (topics[randomTopic] === "Git") {
         console.log("Let's study Git");
-    } else if (topic === "Javascript") {
+    } else if (topics[randomTopic] === "Javascript") {
         console.log("Let's study Javascript");
     } else {
         console.log("Try Again");
@@ -22,3 +21,11 @@ function listTopics() {
         console.log(topics[x]);
     }
 }
+
+var randomTopic = Math.floor(Math.random() * topics.length);
+
+console.log("Here are the topics we are going to study:");
+listTopics();
+
+console.log("What topic should we study?");
+selectTopic();
